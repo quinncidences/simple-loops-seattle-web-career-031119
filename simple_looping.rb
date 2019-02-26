fce1e130746c8156c30a92942a843261530f8e91
Quinn Cox <qrc.inpa@gmail.com>
2019-02-26 02:02:38 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -4,9 +4,8 @@ def loop_iterator(number_of_times)
   counter = 0
   loop do
   puts "Welcome to Flatiron School's Web Development Course!"
+  break if counter == number_of_times
   counter += 1
-  counter == number_of_times
-  break
 end
 end
 end
